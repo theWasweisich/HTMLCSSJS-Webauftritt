9b74eee68c75b2088f8e5bd5c601b51b78115349
theWasweisich <86789640+theWasweisich@users.noreply.github.com>
2025-03-21 15:34:03 +0100

    ok, message requests work now

--- src/admin/script.js
+++ src/admin/script.js
@@ -12,8 +12,45 @@ class MessagesViewer {
     constructor(messages, displayContainer) {
         this.messages = messages;
         this.displayContainer = displayContainer;
+        this.messagesElems = [];
+        this.setup();
     }
     setup() {
+        console.groupCollapsed("Messages");
+        console.log(this.messages);
+        console.groupEnd();
+        for (const message of this.messages) {
+            console.count("Message");
+            let toappend = this.setTemplateFields(message);
+            let done = this.displayContainer.appendChild(toappend);
+            console.log(done);
+            this.messagesElems.push(done);
+        }
+    }
+    setTemplateFields(message) {
+        const template = document.getElementById("message-template");
+        const clone = template.content.cloneNode(true);
+        const toappend = clone.querySelector(".message");
+        let insertableElements = clone.querySelectorAll("[data-field]");
+        insertableElements.forEach(elem => {
+            if (elem.dataset.field === "name") {
+                elem.innerText = `${message.name}, ${message.prename}`;
+            }
+            else if (elem.dataset.field === "email") {
+                elem.innerText = message.email;
+                elem.href = `mailto:${message.email}`;
+            }
+            else if (elem.dataset.field === "topic") {
+                elem.innerText = message.topic;
+            }
+            else if (elem.dataset.field === "short") {
+                elem.innerText = message.shortMsg;
+            }
+            else if (elem.dataset.field === "message") {
+                elem.value = message.longMsg;
+            }
+        });
+        return toappend;
     }
 }
 function getContactMessages() {
@@ -22,16 +59,27 @@ function getContactMessages() {
         return resp;
     });
 }
-function parseResponse(json_) {
+/**
+ * Parses the requested Messages
+ * @param json_ The response taken from getContactMessages().json()
+ * @returns Every received Message
+ */
+function parseMessagesResponse(json_) {
     let msgs = new Array;
+    let timestamps = Object.keys(json_);
+    for (const timestamp of timestamps) {
+        let msg = json_[timestamp];
+        msg.timestamp = new Date(timestamp);
+        msgs.push(msg);
+    }
     return msgs;
 }
 let contactsSection = document.getElementById("request-contacts");
 let requestButton = contactsSection.querySelector("button");
+const msgsOutputSection = document.getElementById("contactMsg-output");
+var viewer;
 requestButton.addEventListener('click', (ev) => __awaiter(void 0, void 0, void 0, function* () {
-    let msgs = yield getContactMessages();
-    console.log(msgs);
-    let output = document.createElement("p");
-    output.innerText = JSON.stringify((yield msgs.json()));
-    contactsSection.appendChild(output);
+    let msgs = yield (yield getContactMessages()).json();
+    let messages = parseMessagesResponse(msgs);
+    viewer = new MessagesViewer(messages, msgsOutputSection);
 }));
--- src/admin/script.ts
+++ src/admin/script.ts
@@ -1,5 +1,5 @@
-
 interface contactMessage {
+    timestamp: Date,
     name: string,
     prename: string,
     email: string,
@@ -8,14 +8,56 @@ interface contactMessage {
     longMsg: string
 }
 
+interface contactMessages {
+    [timestamp: string]: contactMessage
+}
+
 class MessagesViewer {
+    public messagesElems: Array<HTMLElement>;
+
     constructor (
         public messages: Array<contactMessage>,
         public displayContainer: HTMLElement
-    ) {  }
+    ) {
+        this.messagesElems = [];
+        this.setup();
+    }
 
-    private setup() {
+    public setup() {
+        console.groupCollapsed("Messages");
+        console.log(this.messages);
+        console.groupEnd();
+        for (const message of this.messages) {
+            console.count("Message");
+            let toappend = this.setTemplateFields(message);
+            let done = this.displayContainer.appendChild(toappend);
+            console.log(done);
+            this.messagesElems.push(done);
+        }
+    }
+    
+    private setTemplateFields(message: contactMessage): HTMLElement {
+        const template = document.getElementById("message-template") as HTMLTemplateElement;
+        const clone = template.content.cloneNode(true) as HTMLElement;
+        const toappend = clone.querySelector(".message") as HTMLElement;
 
+        let insertableElements = clone.querySelectorAll("[data-field]") as NodeListOf<HTMLElement>;
+        insertableElements.forEach(elem => {
+            if (elem.dataset.field === "name") {
+                elem.innerText = `${message.name}, ${message.prename}`
+            } else if (elem.dataset.field === "email") {
+                elem.innerText = message.email;
+                (elem as HTMLAnchorElement).href = `mailto:${message.email}`;
+            } else if (elem.dataset.field === "topic") {
+                elem.innerText = message.topic;
+            } else if (elem.dataset.field === "short") {
+                elem.innerText = message.shortMsg;
+            } else if (elem.dataset.field === "message") {
+                (elem as HTMLTextAreaElement).value = message.longMsg;
+            }
+        });
+
+        return toappend;
     }
 }
 
@@ -24,20 +66,33 @@ async function getContactMessages() {
     return resp;
 }
 
-function parseResponse(json_: object): Array<contactMessage> {
+/**
+ * Parses the requested Messages
+ * @param json_ The response taken from getContactMessages().json()
+ * @returns Every received Message
+ */
+function parseMessagesResponse(json_: Record<string, any>): Array<contactMessage> {
     let msgs = new Array<contactMessage>;
+
+    let timestamps = Object.keys(json_);
+
+    for (const timestamp of timestamps) {
+        let msg = json_[timestamp] as contactMessage;
+        msg.timestamp = new Date(timestamp);
+        msgs.push(msg);
+    }
+
     return msgs
 }
 
 let contactsSection = document.getElementById("request-contacts") as HTMLElement;
 let requestButton = contactsSection.querySelector("button") as HTMLButtonElement;
-
+const msgsOutputSection = document.getElementById("contactMsg-output") as HTMLElement;
+var viewer: MessagesViewer;
 
 requestButton.addEventListener('click', async ev => {
-    let msgs = await getContactMessages();
-    console.log(msgs);
+    let msgs = await (await getContactMessages()).json();
 
-    let output = document.createElement("p");
-    output.innerText = JSON.stringify((await msgs.json()))
-    contactsSection.appendChild(output);
+    let messages = parseMessagesResponse(msgs);
+    viewer = new MessagesViewer(messages, msgsOutputSection);
 });
